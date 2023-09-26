712e11ddf9cc11796648232ac129fe40c8acfd78
malak jabr <malakjabr2@gmail.com>
2023-09-26 23:51:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let charge 
+  if (distance<400) { 
+    charge = 'This one is on me!'
+    return charge
+  } else if ( distance<2000 && distance>=400){
+    charge = 'That will be twenty bucks.'
+    return charge
+  } else if ( distance>=2000 && distance<2500) {
+    charge= 'I will gladly take your thirty bucks.'
+    return charge
+  } else if (distance>=2500) {
+    charge = 'No can do.'
+    return charge
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let destination 
+  if ( city === "NYC") {
+    destination = "Ok, sounds good."
+    return destination
+  } else {
+    destination ="No go."
+    return destination
+  }
+  
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip( tip){
+  let respond 
+  if ( tip === "generous" ) {
+    respond = "Thank you so much."
+    return respond
+  } else if ( tip === "not as generous") {
+    respond = "Thank you."
+    return respond
+  } else {
+    respond = "Bye."
+    return respond
+  }
   // Write your code here!
 }
